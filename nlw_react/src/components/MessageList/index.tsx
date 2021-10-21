b62f83dccdaa5f55b40521033c8936a352548664
Gabriel Cavalheiro <gabriel.cavalheiromr@gmail.com>
2021-10-21 15:40:45 -0300

    Estilização da Home

---
+++ nlw_react/src/components/MessageList/index.tsx
@@ -0,0 +1,49 @@
+import styles from './style.module.scss';
+import logoImg from '../../assets/logo.svg';
+
+
+export function MessageList() {
+    return (
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.MessageList}>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamos pra cima !!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/gabriel-cavalheiro.png" alt="Gabriel Cavalheiro" />
+                        </div>
+
+                        <span>Gabriel Cavalheiro</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamos pra cima !!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/gabriel-cavalheiro.png" alt="Gabriel Cavalheiro" />
+                        </div>
+
+                        <span>Gabriel Cavalheiro</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamos pra cima !!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/gabriel-cavalheiro.png" alt="Gabriel Cavalheiro" />
+                        </div>
+
+                        <span>Gabriel Cavalheiro</span>
+                    </div>
+                </li>
+                
+
+            </ul>
+        </div>
+    )
+}
